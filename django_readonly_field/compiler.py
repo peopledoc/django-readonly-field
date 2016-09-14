1c37b143d8615c81f95f1f0ca5cc206c270fef9f
Joachim Jablon <joachim.jablon@people-doc.com>
2016-09-14 12:07:42 +0200

    Rewrote the generator as a function to avoid already-consumed errors

--- django_readonly_field/compiler.py
+++ django_readonly_field/compiler.py
@@ -49,6 +49,11 @@ class SQLUpdateCompiler(ReadOnlySQLCompilerMixin, BaseSQLUpdateCompiler):
 
 class SQLInsertCompiler(ReadOnlySQLCompilerMixin, BaseSQLInsertCompiler):
 
+    def _exclude_readonly_fields(self, fields, read_only_field_names):
+        for field in fields:
+            if field.name not in read_only_field_names:
+                yield field
+
     def remove_read_only_fields(self, read_only_field_names):
         """
         Remove the fields from the query which correspond to a
@@ -56,12 +61,9 @@ class SQLInsertCompiler(ReadOnlySQLCompilerMixin, BaseSQLInsertCompiler):
         """
         fields = self.query.fields
 
-        new_fields = (
-            field for field in fields
-            if field.name not in read_only_field_names
-        )
         try:
-            fields[:] = new_fields
+            fields[:] = self._exclude_readonly_fields(
+                fields, read_only_field_names)
         except AttributeError:
             # When deserializing, we might get an attribute error because this
             # list shoud be copied first :
@@ -70,4 +72,5 @@ class SQLInsertCompiler(ReadOnlySQLCompilerMixin, BaseSQLInsertCompiler):
             # should never be mutated. If you want to manipulate this list for
             # your own use, make a copy first."
 
-            self.query.fields = list(new_fields)
+            self.query.fields = list(self._exclude_readonly_fields(
+                fields, read_only_field_names))
