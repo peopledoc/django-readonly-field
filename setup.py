547ab9ede8eda416ea54216f9d8ba7c68b9bb3a1
mrjmad <jmarmand@hybird.org>
2018-03-01 16:25:28 +0100

    Add support (in tox) for Python 3.6. Add PG configuration for Travis.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -63,5 +64,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
