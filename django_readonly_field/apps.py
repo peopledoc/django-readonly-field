30edccb0d34f19c1ebd88e7a78b4c7e4bd743e3f
Eve Le Cellier <eve.le-cellier@people-doc.com>
2018-04-26 15:41:11 +0200

    Fix connections for test envs with multi-db

--- django_readonly_field/apps.py
+++ django_readonly_field/apps.py
@@ -7,16 +7,16 @@ class Readonly(AppConfig):
     name = 'django_readonly_field'
 
     def ready(self):
-        from django.db import connection
+        from django.db import connections
         from django.db import utils
 
         readonly_compiler_module = "django_readonly_field.compiler"
 
-        # Change the current value (this is mostly important for the tests)
-        connection.ops.compiler_module = readonly_compiler_module
+        # Change the current values (this is mostly important for the tests)
+        for c in connections:
+            connections[c].ops.compiler_module = readonly_compiler_module
 
         original_load_backend = utils.load_backend
-
         def custom_load_backend(*args, **kwargs):
             backend = original_load_backend(*args, **kwargs)
 
