512c0217363d9e8d82cd55226f031a8c20f74dfe
Joachim Jablon <joachim.jablon@people-doc.com>
2019-04-23 11:20:49 +0200

    Prepare new version

--- django_readonly_field/__init__.py
+++ django_readonly_field/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.5"
+__version__ = "1.0.6dev0"
 
 default_app_config = "django_readonly_field.apps.Readonly"
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '1.0.5'
+version = '1.0.6dev0'
 
 if sys.argv[-1] == 'publish':
     try:
