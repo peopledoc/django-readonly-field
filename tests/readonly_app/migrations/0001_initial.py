2e06f9cee0ef684be04f0632e8de33f8c5320530
Joachim Jablon <joachim.jablon@people-doc.com>
2022-01-17 15:09:03 +0100

    Fix migrations

--- tests/readonly_app/migrations/0001_initial.py
+++ tests/readonly_app/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Car',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.BigAutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('wheel_number', models.IntegerField()),
                 ('manufacturer', models.CharField(max_length=100)),
             ],
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Book',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.BigAutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('ref', models.IntegerField()),
                 ('iban', models.CharField(max_length=100)),
                 ('name', models.CharField(max_length=250)),
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Bus',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.BigAutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('wheel_number', models.IntegerField()),
                 ('manufacturer', models.CharField(max_length=100)),
             ],
